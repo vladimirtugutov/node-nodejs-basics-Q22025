cfb15e9cc8800725cde1088f53def23e38e9c76b
marmot T <marmot@marmots-MacBook-Air.local>
2025-04-28 18:08:12 -0300

    feat: add read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    const data = await readFile(filePath, { encoding: 'utf-8' });
+    console.log(data);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
