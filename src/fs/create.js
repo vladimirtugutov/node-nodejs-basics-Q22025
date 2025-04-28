120f3720c9e39f3b6f144c32b4253f02d2804681
marmot T <marmot@marmots-MacBook-Air.local>
2025-04-28 17:55:08 -0300

    init: initial commit

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { writeFile, access } from 'fs/promises';
+import { constants } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fresh.txt');
+
+  try {
+    await access(filePath, constants.F_OK);
+    throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+    } else {
+      throw new Error('FS operation failed');
+    }
+  }
 };
 
 await create();
