55c882de4a968df7f3ce6132e660886f434ef68f
marmot T <marmot@marmots-MacBook-Air.local>
2025-04-28 18:51:49 -0300

    feat: add hash, modules, streams

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+  const input = createReadStream(filePath);
+
+  await pipeline(
+    input,
+    async function* (source) {
+      for await (const chunk of source) {
+        hash.update(chunk);
+      }
+    }
+  );
+
+  console.log(hash.digest('hex'));
 };
 
 await calculateHash();
