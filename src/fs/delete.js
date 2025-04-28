2bb1cd3ec2ca71ee57f02ff7d5be34c005173f3f
marmot T <marmot@marmots-MacBook-Air.local>
2025-04-28 18:12:57 -0300

    feat: add delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { unlink } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await unlink(filePath);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
